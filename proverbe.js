f02c1756ac0fd779864ea7e83074a7cddae9c6bb
jigsaw2510 <cata_bb92@yahoo.com>
2022-12-04 10:10:25 +0200

    added proverbe.js file and replaced quotes with proverbs

---
+++ proverbe.js
@@ -0,0 +1,119 @@
+const listaProverbe = [
+    {
+        text:"A aduce apă după ce s-a stins focul."
+    },
+    {
+        text:"A aduna nuiele pentru spinarea sa."
+    },
+    {
+        text:"A ajunge cuțitul la os."
+    },
+    {
+        text:"A auzit clopotul, dar nu știe de la ce bisericǎ."
+    },
+    {
+        text:"A avea ac de cojocul cuiva."
+    },
+    {
+        text:"A avea mai mult noroc decât minte."
+    },
+    {
+        text:"A bate apa-n piuă."
+    },
+    {
+        text:"A bate calul care trage."
+    },
+    {
+        text:"A bate fierul cât e cald."
+    },
+    {
+        text:"A bate la ochi."
+    },
+    {
+        text:"A bate toba în tot satul."
+    },
+    {
+        text:"A călca pe bec."
+    },
+    {
+        text:"A cânta cuiva în strună."
+    },
+    {
+        text:"A căra apă cu ciurul."
+    },
+    {
+        text:"Aceeași Mărie cu altă pălărie."
+    },
+    {
+        text:"Acela care Îl iubește pe Dumnezeu, îi iubește și pe oameni."
+    },
+    {
+        text:"A da bir cu fugiții."
+    },
+    {
+        text:"A da peste noroc."
+    },
+    {
+        text:"A da perle la porci."
+    },
+    {
+        text:"A despica firul în patru."
+    },
+    {
+        text:"A duce cu preșul."
+    },
+    {
+        text:"Adevărul este întotdeauna la mijloc."
+    },
+    {
+        text:"Adevărul se spune la despărțire."
+    },
+    {
+        text:"Adevărul stă scris printre rânduri."
+    },
+    {
+        text:"A dispărut de parcă l-a înghițit pământul."
+    },
+    {
+        text:"A face bortă în apă."
+    },
+    {
+        text:"A face cruce în tavan."
+    },
+    {
+        text:"A face cuiva hatârul."
+    },
+    {
+        text:"A face cum îl taie capul."
+    },
+    {
+        text:"A face din cal măgar și din țânțar armăsar."
+    },
+    {
+        text:"A face ochi dulci cuiva."
+    },
+    {
+        text:"A face pe cineva cu ou și cu oțet."
+    },
+    {
+        text:"A face pe cineva de două parale."
+    },
+    {
+        text:"A face treaba în doi peri."
+    },
+    {
+        text:"A face umbră pământului degeaba."
+    },
+    {
+        text:"A face un bine înseamnă a-și bate cuie în talpă."
+    },
+    {
+        text:"A fi cu ochii în patru."
+    },
+    {
+        text:"A fi pe drojdie (cu banii)."
+    },
+    {
+        text:"A fi prins cu cioara vopsită."
+    },
+]
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
@@ -9,7 +8,8 @@ const loader = document.getElementById('loader');
 
 
 
-let apiQuotes = [];
+
+// let apiQuotes = [];
 
 // Show Loading
 function loading() {
@@ -27,13 +27,7 @@ function complete() {
 function newQuote() {
     loading();
     // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Check if author field is blank and replace it with 'Unknown'
-    if (!quote.author) {
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
+    const quote = listaProverbe[Math.floor(Math.random() * listaProverbe.length)];
     // Check Quote length to determine styling
     if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
@@ -47,22 +41,22 @@ function newQuote() {
 
 // Get Quotes from API
 
-async function getQuotes() {
-    loading();
-    // const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error) {
-        // Catch Error Here
-    }
-}
+// async function getQuotes() {
+//     loading();
+//     // const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
 
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}`;
     window.open(twitterUrl, '_blank');
 }
 
@@ -71,7 +65,9 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
-getQuotes();
+// getQuotes();
+newQuote();
+console.log(listaProverbe[1].text);
 
 
 
